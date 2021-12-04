1c46207a0683a5b95211e5983d9a4ed6a4c1bee9
OneFastBoi <peta.sebela@gmail.com>
2021-12-04 13:22:58 +0100

    database commit

--- database.py
+++ database.py
@@ -1,3 +1,15 @@
 import json
+from os import path, write
 
 STRUCTURE = "database/structure.json"
+class database():
+    def __init__(self,config) -> None:
+        self.config = json.load(open(config))
+        self.path = self.config["path"]
+        self.files = self.config["files"]
+
+    def write(self,name,data=None):
+        file = open(self.path + name + '.json',"w")
+
+database = database("config.json")
+database.write("zstgm")
