8667662d0cca41f8ce1a1e83ab4164bdf078fbb4
OneFastBoi <peta.sebela@gmail.com>
2021-12-15 20:05:59 +0100

    front-end

--- static/javascript/logout.js
+++ static/javascript/logout.js
@@ -1,25 +1,36 @@
 const notification = document.getElementById("notification-board");
 const board = document.getElementById("board");
-
+const logOut = document.getElementById("logout");
+const displayName = document.getElementById("name"); 
 var state = false
+
 login.addEventListener("click", () => {
      if (!state) {
           state = true;
           notification.classList.remove("hide");
+          login.classList.add("wide");
           board.classList.add("shade");
+          displayName.innerText = "User";
      }
 
      else {
           state = false;
           notification.classList.add("hide");
           board.classList.remove("shade");
+          login.classList.remove("wide");
+          displayName.innerText = "U";
      }
 });
 
 board.addEventListener("click", (event) => {
+     console.log("s")
      if (event.target != login && state) {
           state = false;
           notification.classList.add("hide");
           board.classList.remove("shade");
      }
 });
+
+logOut.addEventListener("click", ()=>{
+     console.log("logout");
+})
