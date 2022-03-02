c667faf454c2a247904ee9275ea6ef5bc342961f
OneFastBoi <peta.sebela@gmail.com>
2022-03-02 21:56:06 +0100

    management

--- static/javascript/table.js
+++ static/javascript/table.js
@@ -10,17 +10,29 @@ const target = document.querySelector(".table-container");
 
 function generateTable(name, score) {
      let table = document.createElement("form");
+     let header = document.createElement("h2");
+     header.textContent = name;
+     table.appendChild(header);
+
+
+     let inputContainer = document.createElement("div");
      for (let e = 0; e < score; e++) {
-          var scoreValue = document.createElement("p") 
-          var input = document.createElement("input")
+          let row = document.createElement("div");
+          var scoreValue = document.createElement("p");
+          var input = document.createElement("input");
+          input.type = "number"
           scoreValue.textContent = e.toString();
-          table.appendChild(scoreValue);
-          table.appendChild(input);
+          row.appendChild(scoreValue);
+          row.appendChild(input);
+          inputContainer.appendChild(row);
      }
+
      table.classList.add("table")
+     table.appendChild(inputContainer);
      target.appendChild(table);
 }
 
-category.forEach(element => {
-     generateTable(element,20);
-});
+// category.forEach(element => {
+//      generateTable(element,120);
+// });
+generateTable("element",120);
