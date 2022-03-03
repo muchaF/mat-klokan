1b23d1ccf2273bdf7c98e1d621ba31805739c83a
Petr Šebela <58368455+OneFastBoi@users.noreply.github.com>
2022-03-03 22:16:59 +0100

    a

--- static/javascript/table.js
+++ static/javascript/table.js
@@ -6,33 +6,18 @@ const category = ["Cvrček",
                   "Junior",
                   "Student"]
 
-const target = document.querySelector(".table-container");
+const years = document.querySelectorAll(".category-container")
+console.log(years)
 
-function generateTable(name, score) {
-     let table = document.createElement("form");
-     let header = document.createElement("h2");
-     header.textContent = name;
-     table.appendChild(header);
-
-
-     let inputContainer = document.createElement("div");
-     for (let e = 0; e < score; e++) {
-          let row = document.createElement("div");
-          var scoreValue = document.createElement("p");
-          var input = document.createElement("input");
-          input.type = "number"
-          scoreValue.textContent = e.toString();
-          row.appendChild(scoreValue);
-          row.appendChild(input);
-          inputContainer.appendChild(row);
+function fillYear(year){
+     for (let catIndex = 0; catIndex < category.length; catIndex++) {
+          var cat = document.createElement("div");
+          cat.textContent = category[catIndex];
+          cat.classList.add("cetegory");
+          year.appendChild(cat);
      }
-
-     table.classList.add("table")
-     table.appendChild(inputContainer);
-     target.appendChild(table);
 }
 
-// category.forEach(element => {
-//      generateTable(element,120);
-// });
-generateTable("element",120);
+years.forEach(year => {
+     fillYear(year);
+});
